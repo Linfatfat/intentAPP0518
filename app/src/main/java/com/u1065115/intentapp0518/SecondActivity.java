5f8426bec9918b34f3de11cd13137c2bf49f17ae
Linfatfat <s940110118@gmail.com>
2020-05-18 10:46:11 +0800

    完成專案

--- app/src/main/java/com/u1065115/intentapp0518/SecondActivity.java
+++ app/src/main/java/com/u1065115/intentapp0518/SecondActivity.java
@@ -1,20 +1,19 @@
 package com.u1065115.intentapp0518;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 
-    public void btnOK(View view) {
-        Intent intent = new Intent(this,SecondActivity.class);
-        startActivity(intent);
+    public void btnclose(View view) {
+        finish();
     }
+
 }
